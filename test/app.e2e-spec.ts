6bc982ab4a0546c5ba92e82140050ef07eaa9006
DatND <datnd@DatND.local>
2023-10-17 10:38:31 +0700

    feat/login

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,9 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import 'dotenv/config';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { app } from './constants';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication;
-
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
-
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+describe('ROOT', () => {
+  it('should ping', () => {
+    return request(app).get('/').expect(200).expect('Hello World!');
   });
 });
